b8bf2915f8e929d55ec386ac6807dadb39c31b53
Vicky Rodriguez <vrod8763@gmail.com>
2023-01-12 13:41:08 -0500

    add decrement

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -9,6 +9,9 @@ const countReducer =(state, action) => {
   case 'INCREMENT': {
       return {count: state.count + action.step}
   }
+  case 'DECREMENT': {
+    return {count: state.count - action.step}
+}
   default:{
     throw new Error(`Unsupported action type: ${action.type}`)
   }
@@ -19,7 +22,10 @@ function Counter({initialCount = 0, step = 1}) {
   const [state, dispatch] = React.useReducer(countReducer, {count: initialCount})
   const {count} = state
   const increment = () => dispatch({type: 'INCREMENT', step})
-  return <button onClick={increment}>{count}</button>
+  const decrement = () => dispatch({type: 'DECREMENT', step})
+  return <><button onClick={increment}>Increment</button>{count}
+  <button onClick={decrement}>Decrement</button>
+  </>
 }
 
 function App() {
